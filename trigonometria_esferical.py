148fcb2abc799fa645f75cf388420bd72f1b5d5f
mondeja <mondejar1994@gmail.com>
2017-03-21 12:46:50 +0100

    Update trigonometria_esferical.py

--- trigonometria_esferical.py
+++ trigonometria_esferical.py
@@ -5,7 +5,7 @@ import math as m
 # Este es  el módulo que permite calcular cuanto debería esconderse el horizonte
 # bajo la curvatura de la Tierra si esta fuera una bola
 
-L = input('Introduce la distancia: ')
+L = input('Introduce la distancia (en Kms): ')
 response = 6373-(6373*(m.cos(m.asin(float(L)/6373))))
 response = response * 1000
 print 'El horizonte se esconde : ' + str(response) + ' metros en una esfera'
