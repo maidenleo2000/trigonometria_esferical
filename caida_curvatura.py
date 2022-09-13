6f568c7501f40fefc112c01c4530a97db2e6c56b
Leandro <maidenleo2000@gmail.com>
2022-09-12 21:02:42 -0300

    Agregado boton borrar. Modificado msj error

--- caida_curvatura.py
+++ caida_curvatura.py
@@ -13,15 +13,18 @@ response *= 1000
 print (f'El horizonte se esconde  {str(response)} metros en una esfera')
 """
 
+def limpiarDatos():
+    label = ttk.Label(text="                                                                                                                 ")        
+    label.place(x=25, y=100)
 
 def apretarboton():
     dato=ingdatos.get()
 
     if dato == "":
         
-        messagebox.showinfo(title="¡Advertencia!", message="No se ingresaron datos")
-        label = ttk.Label(text="                                     ")        
-        label.place(x=10, y=35)
+        # messagebox.showinfo(title="¡Advertencia!", message="No se ingresaron datos")
+        label = ttk.Label(text="No se ingresaron datos                                                                           ")        
+        label.place(x=25, y=100)
         
     else:
         
@@ -30,23 +33,26 @@ def apretarboton():
         response *= 1000
         resultado = float("{0:.2f}".format(response))
         
-        label = ttk.Label(text=f"El horizonte se esconde  {str(resultado)} metros en una esfera           ")
+        label = ttk.Label(text=f"El horizonte se esconde  {str(resultado)} metros en una esfera")
         label.place(x=25, y=100)
         
     
 
 ventana = tk.Tk()
 ventana.title("Calcular caída de curvatura")
-ventana.config(width=350, height=230)
+ventana.config(width=400, height=250)
 
 label = ttk.Label(text="Ingresa la distancia en KM")
-label.place(x=100, y=7)
+label.place(x=140, y=7)
 
 ingdatos = ttk.Entry()
-ingdatos.place(x=109, y=30)
+ingdatos.place(x=145, y=30)
 ingdatos.config(width=20, )
 
 boton = ttk.Button(text="Calcular", command=apretarboton)
-boton.place(x=109, y=60)
+boton.place(x=120, y=60)
+
+botonCancelar = ttk.Button(text="Limpiar Datos", command=limpiarDatos)
+botonCancelar.place(x=220, y=60)
 
 ventana.mainloop()
